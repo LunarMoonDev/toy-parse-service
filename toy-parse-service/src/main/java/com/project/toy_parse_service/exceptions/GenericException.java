ec26bf44b75c2ab297b19ea4d4ffaea41fa60759
Harjit Brar <workandresearchbrar@gmail.com>
2024-08-02 14:41:19 +0800

    feat: added exception handler

---
+++ toy-parse-service/src/main/java/com/project/toy_parse_service/exceptions/GenericException.java
@@ -0,0 +1,18 @@
+package com.project.toy_parse_service.exceptions;
+
+import com.project.toy_parse_service.enums.Errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GenericException extends RuntimeException{
+    private String code;
+    private String message;
+
+    public GenericException(Errors error) {
+        this.code = error.getCode();
+        this.message = error.getMessage();
+    }
+}
