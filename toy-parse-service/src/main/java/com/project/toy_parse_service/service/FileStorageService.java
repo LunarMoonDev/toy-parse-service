5cdd6fdb04fe3a9eb3ac08c09c8c55439b82703b
Harjit Brar <workandresearchbrar@gmail.com>
2024-08-06 17:27:18 +0800

    feat: added service method to grab files from gdrive

--- toy-parse-service/src/main/java/com/project/toy_parse_service/service/FileStorageService.java
+++ toy-parse-service/src/main/java/com/project/toy_parse_service/service/FileStorageService.java
@@ -1,11 +1,15 @@
 package com.project.toy_parse_service.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.project.toy_parse_service.dto.parse.ReportsDTO;
 
 import reactor.core.publisher.Mono;
 
 public interface FileStorageService {
     Mono<String> upload(String uuid, MultipartFile file) throws IOException;
+    Mono<List<ReportsDTO>> list(String uuid);
 }
