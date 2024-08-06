ebebf9ce4d22d5dbdaa0cc6bfad8b8f1a920a8bb
Harjit Brar <workandresearchbrar@gmail.com>
2024-08-06 17:28:00 +0800

    feat: added service to grab csv files

--- toy-parse-service/src/main/java/com/project/toy_parse_service/service/ParseService.java
+++ toy-parse-service/src/main/java/com/project/toy_parse_service/service/ParseService.java
@@ -6,6 +6,7 @@ import java.math.BigInteger;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.toy_parse_service.dto.parse.response.ParseResponseDTO;
+import com.project.toy_parse_service.dto.parse.response.ReportsResponseDTO;
 import com.project.toy_parse_service.exceptions.GenericException;
 
 import reactor.core.publisher.Mono;
@@ -13,5 +14,6 @@ import reactor.core.publisher.Mono;
 public interface ParseService {
     String DATE_PATTERN = "YYYY_MM_DD";
 
-    Mono<ParseResponseDTO> save(String uuid, MultipartFile payload, BigInteger id) throws GenericException,IOException;
+    Mono<ParseResponseDTO> save(String uuid, MultipartFile payload, BigInteger id) throws GenericException, IOException;
+    Mono<ReportsResponseDTO> list(String uuid) throws GenericException, IOException;
 }
--- toy-parse-service/src/main/java/com/project/toy_parse_service/service/impl/ParseServiceImpl.java
+++ toy-parse-service/src/main/java/com/project/toy_parse_service/service/impl/ParseServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.toy_parse_service.dto.parse.response.ParseResponseDTO;
+import com.project.toy_parse_service.dto.parse.response.ReportsResponseDTO;
 import com.project.toy_parse_service.enums.Errors;
 import com.project.toy_parse_service.enums.Success;
 import com.project.toy_parse_service.exceptions.GenericException;
@@ -23,7 +24,7 @@ import reactor.core.publisher.Mono;
 
 @Service("ParseService_STABLE")
 @Slf4j
-public class ParseServiceImpl implements ParseService{
+public class ParseServiceImpl implements ParseService {
     @Autowired
     private FileStorageService service;
 
@@ -31,7 +32,8 @@ public class ParseServiceImpl implements ParseService{
     private ReportsRepository repository;
 
     @Override
-    public Mono<ParseResponseDTO> save(String uuid, MultipartFile payload, BigInteger id) throws GenericException, IOException {
+    public Mono<ParseResponseDTO> save(String uuid, MultipartFile payload, BigInteger id)
+            throws GenericException, IOException {
         log.info("X-Tracker: {} | Uploading report for given request...", uuid);
         log.debug("X-Tracker: {} | request payload: {}", payload);
 
@@ -43,10 +45,10 @@ public class ParseServiceImpl implements ParseService{
 
         // check csv
         log.info("X-Tracker: {} | Checking for content type...", uuid);
-        if(!payload.getContentType().equalsIgnoreCase("text/csv")) {
+        if (!payload.getContentType().equalsIgnoreCase("text/csv")) {
             throw new GenericException(Errors.PARSE_TYPE_ERROR);
         }
-        
+
         boolean isInvalid = false;
         // check file name
         log.info("X-Tracker: {} | Checking for file name format...", uuid);
@@ -62,25 +64,38 @@ public class ParseServiceImpl implements ParseService{
                 isInvalid = true;
             }
 
-            if(!restString.equals("_report.csv")) {
+            if (!restString.equals("_report.csv")) {
                 isInvalid = true;
             }
         }
 
-        if(isInvalid) {
+        if (isInvalid) {
             throw new GenericException(Errors.PARSE_FILE_NAME_ERROR);
         }
-        
 
         return service.upload(uuid, payload)
-            .map(reportId -> MapperUtil.toReports(id, reportId))
-            .doOnNext(report -> repository.save(report))
-            .doOnNext(report -> log.info("X-Tracker: {} | Success in saving report in database.", uuid))
-            .map(report -> MapperUtil.toPlayerResponseDTO(Success.PARSE_UPLOAD_SUCCESS))
-            .doOnError(error -> {
-                log.error("X-Tracker: {} | specific exception: {}", uuid, error.getMessage());
+                .map(reportId -> MapperUtil.toReports(id, reportId))
+                .doOnNext(report -> repository.save(report))
+                .doOnNext(report -> log.info("X-Tracker: {} | Success in saving report in database.", uuid))
+                .map(report -> MapperUtil.toPlayerResponseDTO(Success.PARSE_UPLOAD_SUCCESS))
+                .doOnError(error -> {
+                    log.error("X-Tracker: {} | specific exception: {}", uuid, error.getMessage());
 
-                throw new GenericException(Errors.SERVICE_ERROR);
-            });
+                    throw new GenericException(Errors.SERVICE_ERROR);
+                });
+    }
+
+    @Override
+    public Mono<ReportsResponseDTO> list(String uuid) throws GenericException, IOException {
+        log.info("X-Tracker: {} | Retrieving list of reports uploaded", uuid);
+
+        try {
+            return service.list(uuid)
+                    .map(MapperUtil::toReportsResponseDTO);
+        } catch (RuntimeException exception) {
+            log.error("X-Tracker: {} | specific exception: {}", uuid, exception.getMessage());
+
+            throw new GenericException(Errors.SERVICE_ERROR);
+        }
     }
 }
